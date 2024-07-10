b153856bab48224619baede21ecbf35eb5f20501
sarthik <sarthik32@gmail.com>
2024-07-10 12:55:04 +0530

    [Sarthik]Modified: created separate folder

---
+++ Basic Programs/harmonic_number.py
@@ -0,0 +1,14 @@
+# 4. Harmonic Number
+
+def harmonic_number(n):
+    result = 0
+    if(n==0):
+        return "Enter a valid positive integer number"
+    for i in range(1, n + 1):
+        result += 1 / i
+    return result
+
+if __name__ == '__main__':
+
+    num = int(input("Enter the harmonic value: "))
+    print(harmonic_number(num))
