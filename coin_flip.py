b5449823a76b0b3f4d92495bc534084bfa5fb525
sarthik <sarthik32@gmail.com>
2024-07-10 10:31:47 +0530

    [sarthik]ADD: coin flip

--- coin_flip.py
+++ coin_flip.py
@@ -4,7 +4,7 @@ import random
 def coin_flip(num_of_flips):
     heads = 0
     tails = 0
-    for i in range(num_of_flips):
+    for _ in range(num_of_flips):
         coin_toss = random.random()
         if coin_toss < 0.5 :
             tails += 1
@@ -15,8 +15,11 @@ def coin_flip(num_of_flips):
 
     return heads_percentage, tails_percentage
 
-num_of_flips = int(input("Enter number of coin flips: "))
 
-heads_percentage, tails_percentage = coin_flip(num_of_flips)
 
-print(f'heads percentage: {heads_percentage} and tails percentage: {tails_percentage}')
+if __name__ == '__main__':
+    num_of_flips = int(input("Enter number of coin flips: "))
+
+    heads_percentage, tails_percentage = coin_flip(num_of_flips)
+
+    print(f'heads percentage: {heads_percentage} and tails percentage: {tails_percentage}')
