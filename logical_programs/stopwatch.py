61648f6269d0e551e32b047b2e6820de286ec6c6
sarthik <sarthik32@gmail.com>
2024-07-11 11:12:36 +0530

    [Sarthik]ADD: prgm to simulate stopwatch

---
+++ logical_programs/stopwatch.py
@@ -0,0 +1,17 @@
+# 6. Stopwatch simulation 
+import time
+
+def start_stopwatch():
+    return time.time()
+
+def end_stopwatch(start_time):
+    return time.time() - start_time
+
+if __name__ == "__main__":
+    input("Please Enter to start the stopwatch ⏱️")
+    start_time = start_stopwatch()
+
+    input("stop the stopwatch ->⏱️")
+    elapsed_time = end_stopwatch(start_time)
+
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
