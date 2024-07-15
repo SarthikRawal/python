58ac94b45891713c4fdfb5c9bb9b9abbb2c9882d
sarthik <sarthik32@gmail.com>
2024-07-15 17:33:19 +0530

    [Sarthik]ADD: UC-3 created play options for the game

--- advance_problems/snake_and_ladder.py
+++ advance_problems/snake_and_ladder.py
@@ -1,8 +1,22 @@
 # Snake & Ladder Simulator
 import random
 
-def roll_dice():
-    return random.randint(1, 6)
+def snake_ladder():
+    player_position = 0
 
-player_start_position = 0
+    def roll_dice():
+        return random.randint(1, 6)
 
+    def play_option():
+        dice_number = roll_dice()
+        play = random.choice(['no-play', 'ladder', 'snake'])
+        if play == 'no-play':
+            return
+        elif play == 'ladder':
+            player_position += dice_number
+            return player_position
+        else: 
+            player_position -= dice_number
+            return player_position
+        
+    
