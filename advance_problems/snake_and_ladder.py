e01e139946c45bf9384f949972369b52799e7136
sarthik <sarthik32@gmail.com>
2024-07-16 13:35:39 +0530

    [Sarthik]ADD: UC-7 functionalities for two player game added

--- advance_problems/snake_and_ladder.py
+++ advance_problems/snake_and_ladder.py
@@ -4,38 +4,79 @@ import random
 WINNING_POSITION = 100
 
 def roll_dice():
+    """
+    returns integer value 1-6
+    """
     return random.randint(1, 6)
 
 def play_option():
+    """
+    returns the playing options
+    """
     return random.choice(['no-play', 'ladder', 'snake'])
     
 def game():
     start_position = 0
-    player_position = start_position
-    no_of_times_dice_rolled = 0
+    player1_position = start_position
+    player2_position = start_position
+    player1_dice_rolls = 0
+    player2_dice_rolls = 0
+    current_player = 1
 
-    while player_position < WINNING_POSITION:
-        no_of_times_dice_rolled += 1
-        dice_number = roll_dice()
+    while player1_position < WINNING_POSITION and player2_position < WINNING_POSITION:
+        if current_player == 1:
+            player1_dice_rolls += 1
+            dice_number = roll_dice()
 
-        play = play_option()
-        if play == 'no-play':
-            pass
+            play = play_option()
+            if play == 'no-play':
+                pass
         
-        if play == 'ladder':
-            if not player_position + dice_number > WINNING_POSITION:
-                player_position = player_position + dice_number
+            if play == 'ladder':
+                if not player1_position + dice_number > WINNING_POSITION:
+                    player1_position += dice_number
 
-        if play == 'snake':
-            player_position -= dice_number
-    
-            if player_position < start_position:
-                player_position = start_position
+            if play == 'snake':
+                player1_position -= dice_number
+        
+                if player1_position < start_position:
+                    player1_position = start_position
             
-        if player_position == WINNING_POSITION:
-            print(f"Player Position: {player_position}, Player-won 🎉🎉")
+            if player1_position == WINNING_POSITION:
+                print(f"Player 1 Position: {player1_position}, Player-1 won 🎉🎉")
+                break
+            
+            current_player = 2
 
-    print(f"Number of times 🎲 rolled to win: {no_of_times_dice_rolled}")
+        elif current_player == 2:
+            player2_dice_rolls += 1
+            dice_number = roll_dice()
+
+            play = play_option()
+            if play == 'no-play':
+                pass
+        
+            if play == 'ladder':
+                if not player2_position + dice_number > WINNING_POSITION:
+                    player2_position += dice_number
+
+            if play == 'snake':
+                player2_position -= dice_number
+        
+                if player2_position < start_position:
+                    player2_position = start_position
+            
+            if player2_position == WINNING_POSITION:
+                print(f"Player 2 Position: {player2_position}, Player-2 won 🎉🎉")
+                break
+            
+            current_player = 1
+    if player1_position == WINNING_POSITION:
+        print(f"Player 1 won in {player1_dice_rolls} 🎲 rolls")
+    elif player2_position == WINNING_POSITION:
+        print(f"Player 2 won in {player2_dice_rolls} 🎲 rolls")
+        
+    
 
 if __name__ == '__main__':
     # Game execution
