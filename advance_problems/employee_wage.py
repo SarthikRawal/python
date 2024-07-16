fd302c4ef5223e4aa903b99c02e1f95bc135e02f
sarthik <sarthik32@gmail.com>
2024-07-16 16:06:08 +0530

    [Sarthik]ADD: UC-2 calculate daily wages

--- advance_problems/employee_wage.py
+++ advance_problems/employee_wage.py
@@ -7,3 +7,9 @@ def check_attendance():
     """
     return random.randint(0, 1)
 
+def calculate_wage(wage_per_hour = 20, full_day_hour = 8):
+    """
+    calculate daily wage 
+    """
+    daily_wage = wage_per_hour * full_day_hour
+    return daily_wage
