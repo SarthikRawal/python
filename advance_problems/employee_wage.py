276916926fc941bc723fe70e1737fa6b38de08d7
sarthik <sarthik32@gmail.com>
2024-07-17 16:53:01 +0530

    [Sarthik]ADD: UC-8 compute employee wage for multiple companies

--- advance_problems/employee_wage.py
+++ advance_problems/employee_wage.py
@@ -44,15 +44,64 @@ class Employee:
             self.total_days_worked += 1
 
     def get_emp_salary_details(self):
+        print(f"Name: {self.emp_name}")
         print(f"Total Wage: {self.total_wage}") 
         print(f"Total Hours Worked: {self.total_hrs_worked}")
         print(f"Total Days Workde: {self.total_days_worked}")
+
+
+class Company:
+    def __init__(self, name) -> None:
+        self.company_name = name
+        self.employee_dict = {}
+
+    def add_employee(self, emp_obj: Employee):
+        self.employee_dict.update({emp_obj.emp_name: emp_obj})
+
+    def delete_employee(self, emp_obj: Employee):
+        self.employee_dict.pop(emp_obj.emp_name)
+    
+    def display_emp_detail(self):
+        # print(self.employee_dict)
+        for emp in self.employee_dict.values():
+            emp.get_emp_salary_details()
+            print("="*30)
+
+class Multiple_Company:
+    def __init__(self) -> None:
+        # self.company_name = name
+        self.company_dict = {}
+
+    def add_company(self, com_obj: Company):
+        self.company_dict.update({com_obj.company_name: com_obj})
+
+    def delete_company(self, com_obj: Company):
+        self.company_dict.pop(com_obj.company_name)
+
+    def display_company(self):
+        print("Companies:")
+        for com in self.company_dict.values():
+            print(f"Company: {com.company_name}")
 
 
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation")
 
     emp = Employee("sarthik", 20)
-    # daily_wage = emp.daily_wage()
     emp.monthly_wage()
-    emp.get_emp_salary_details()
+    emp1 = Employee("rawal", 30)
+    emp1.monthly_wage()
+    com = Company("Apple")
+    com1 = Company("Google")
+    
+    companies = Multiple_Company()
+    companies.add_company(com)
+    companies.add_company(com1)
+    companies.display_company()
+    print("="*30)
+    com.add_employee(emp)
+    com.add_employee(emp1)
+    com.display_emp_detail()
+
+    # com.delete_employee(emp1)
+    # com.display_emp_detail()
