96b2308e08bc852d3b8b711a2fdee568d9a26123
sarthik <sarthik32@gmail.com>
2024-07-17 12:24:19 +0530

    [Sarthik]ADD: UC-6 calculate wages till it reaches 100 hours or 20 days per month

--- advance_problems/employee_wage.py
+++ advance_problems/employee_wage.py
@@ -5,6 +5,7 @@ WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8
 PART_TIME_HOURS = 4
 WORKING_DAYS_PER_MONTH = 20
+MAX_WORKING_HOURS = 100
 
 def check_attendance():
     """
@@ -18,18 +19,31 @@ def daily_wage(emp_type):
     """
     match emp_type:
         case 0:
-            return "Absent..!"
+            return 0,0 #Absent
         case 1: 
-            return WAGE_PER_HOUR * FULL_DAY_HOURS
+            return WAGE_PER_HOUR * FULL_DAY_HOURS, FULL_DAY_HOURS
         case 2:
-            return WAGE_PER_HOUR * PART_TIME_HOURS
+            return WAGE_PER_HOUR * PART_TIME_HOURS, PART_TIME_HOURS
          
 def monthly_wage():
-    total_wage = 0
+    total_wages = 0
+    total_hours = 0
+    total_days = 0
     attendance = check_attendance()
-    for _ in WORKING_DAYS_PER_MONTH:
-        if attendance == 1:
-            total_wage += daily_wage(1)
-        elif attendance == 2:
-            total_wage += daily_wage(2)
-    return total_wage
+    while total_days < WORKING_DAYS_PER_MONTH and total_hours < MAX_WORKING_HOURS:
+        wage, hours = daily_wage(attendance)
+        
+        total_wages += wage
+        total_hours += hours
+        total_days += 1
+
+    return total_wages, total_hours, total_days
+
+def main():
+    total_wage, total_hours, total_days = monthly_wage()
+    print(f"Total wage: {total_wage}💰")
+    print(f"Total hours worked: {total_hours}")
+    print(f"Total days worked: {total_days}")
+
+if __name__ == "__main__":
+    main()
