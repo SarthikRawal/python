2ce849430681ae206e33e0055df2a1559c4dde17
sarthik <sarthik32@gmail.com>
2024-07-17 15:22:16 +0530

    [Sarthik]ADD: UC-7 Refactor into classes and object programming

--- advance_problems/employee_wage.py
+++ advance_problems/employee_wage.py
@@ -1,49 +1,58 @@
 # Employee Wages
 import random
 
-WAGE_PER_HOUR = 20
-FULL_DAY_HOURS = 8
-PART_TIME_HOURS = 4
-WORKING_DAYS_PER_MONTH = 20
-MAX_WORKING_HOURS = 100
+class Employee:
 
-def check_attendance():
-    """
-    check for employee attendance status
-    """
-    return random.randint(0, 2)
+    FULL_DAY_HOURS = 8
+    PART_TIME_HOURS = 4
+    WORKING_DAYS_PER_MONTH = 25
+    MAX_WORKING_HOURS = 100
 
-def daily_wage(emp_type):
-    """
-    calculate daily and part time wage 
-    """
-    match emp_type:
-        case 0:
-            return 0,0 #Absent
-        case 1: 
-            return WAGE_PER_HOUR * FULL_DAY_HOURS, FULL_DAY_HOURS
-        case 2:
-            return WAGE_PER_HOUR * PART_TIME_HOURS, PART_TIME_HOURS
-         
-def monthly_wage():
-    total_wages = 0
-    total_hours = 0
-    total_days = 0
-    attendance = check_attendance()
-    while total_days < WORKING_DAYS_PER_MONTH and total_hours < MAX_WORKING_HOURS:
-        wage, hours = daily_wage(attendance)
-        
-        total_wages += wage
-        total_hours += hours
-        total_days += 1
+    def __init__(self, name, wage_per_hr):
+        self.emp_name = name
+        self.wage_per_hr = wage_per_hr
+        self.total_hrs_worked = 0
+        self.total_days_worked = 0
+        self.total_wage = 0        
+    
+    @staticmethod    
+    def check_attendance():
+        """
+        check for employee attendance status
+        """
+        return random.randint(0, 2)
+    
+    def daily_wage(self):
+        """
+        calculate daily and part time wage 
+        """
+        emp_status = self.check_attendance()
+        match emp_status:
+            case 0:
+                return 0,0 #Absent
+            case 1: 
+                return self.wage_per_hr * self.FULL_DAY_HOURS, self.FULL_DAY_HOURS
+            case 2:
+                return self.wage_per_hr * self.PART_TIME_HOURS, self.PART_TIME_HOURS
+            
+    def monthly_wage(self):
+        while self.total_days_worked < self.WORKING_DAYS_PER_MONTH and self.total_hrs_worked < self.MAX_WORKING_HOURS:
+            wage, hours = self.daily_wage()
+            
+            self.total_wage += wage
+            self.total_hrs_worked += hours
+            self.total_days_worked += 1
 
-    return total_wages, total_hours, total_days
+    def get_emp_salary_details(self):
+        print(f"Total Wage: {self.total_wage}") 
+        print(f"Total Hours Worked: {self.total_hrs_worked}")
+        print(f"Total Days Workde: {self.total_days_worked}")
 
-def main():
-    total_wage, total_hours, total_days = monthly_wage()
-    print(f"Total wage: {total_wage}💰")
-    print(f"Total hours worked: {total_hours}")
-    print(f"Total days worked: {total_days}")
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    print("Welcome to Employee Wage Computation")
+
+    emp = Employee("sarthik", 20)
+    # daily_wage = emp.daily_wage()
+    emp.monthly_wage()
+    emp.get_emp_salary_details()
