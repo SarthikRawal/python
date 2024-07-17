6ac6a4a2d1b4e13e2673b1054e663f8671c61855
sarthik <sarthik32@gmail.com>
2024-07-17 11:04:34 +0530

    [Sarthik]ADD: UC-3 calculate part time wage

--- advance_problems/employee_wage.py
+++ advance_problems/employee_wage.py
@@ -7,9 +7,13 @@ def check_attendance():
     """
     return random.randint(0, 1)
 
-def calculate_wage(wage_per_hour = 20, full_day_hour = 8):
+def daily_wage(wage_per_hour = 20, full_day_hour = 8):
     """
     calculate daily wage 
     """
     daily_wage = wage_per_hour * full_day_hour
     return daily_wage
+
+def part_time_wage(wage_per_hour = 20, part_time_hour = 4):
+    part_time_wage = wage_per_hour * part_time_hour
+    return part_time_wage
