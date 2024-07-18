506ae6dcbee1b6cfad762df3b460e010d875192f
sarthik <sarthik32@gmail.com>
2024-07-18 11:27:55 +0530

    UC-3 ability to edit existing contact

--- advance_problems/address_book.py
+++ advance_problems/address_book.py
@@ -23,12 +23,38 @@ class Contact:
         print(f"Phone No: {self.phone_no}")
         print(f"Email: {self.email}")
 
+    def update_contact(self, first_name=None, last_name=None, address=None, city=None, state=None, zip=None, phone_no=None, email=None):
+        if first_name:
+            self.first_name = first_name
+        if last_name:
+            self.last_name = last_name
+        if address:
+            self.address = address
+        if city:
+            self.city = city
+        if state:
+            self.state = state
+        if zip:
+            self.zip = zip
+        if phone_no:
+            self.phone_no = phone_no
+        if email:
+            self.email = email
+
 class AddressBook:
     def __init__(self):
         self.contact_dict = {}
 
     def add_contact(self, contact_obj: Contact):
         self.contact_dict[contact_obj.first_name] = contact_obj
+
+    def edit_contact(self, first_name, **info):
+        if first_name in self.contact_dict:
+            contact = self.contact_dict[first_name]
+            contact.update_contact(**info)
+            print(f"--> Contact {first_name} has been updated.\n")
+        else:
+            print(f"--> Contact with first name {first_name} not found.\n")
 
     def display_contact(self):
         for con in self.contact_dict.values():
@@ -37,7 +63,7 @@ class AddressBook:
 
 
 if __name__ == '__main__':
-    print("Welcome to Address Book")
+    print("Welcome to Address Book\n")
 
     address_book = AddressBook()
     
@@ -46,5 +72,7 @@ if __name__ == '__main__':
 
     address_book.add_contact(contact1)
     address_book.add_contact(contact2)
+    # address_book.display_contact()
 
+    address_book.edit_contact('sarthik', last_name = 'Rawal', address = '68/CHB')
     address_book.display_contact()
