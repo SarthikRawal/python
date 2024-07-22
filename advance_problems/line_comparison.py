aa8f0240cadbc5f60a8c97190e945c78a18a0656
sarthik <sarthik32@gmail.com>
2024-07-22 15:17:12 +0530

    [Sarthik]ADD: UC-3 comparing lines to know if they are greater, equal or less than

--- advance_problems/line_comparison.py
+++ advance_problems/line_comparison.py
@@ -19,6 +19,11 @@ class Line:
     def __eq__(self, other):
         return self.length == other.length
     
+    def __gt__(self, other):
+        return self.length > other.length
+    
+    def __lt__(self, other):
+        return self.length < other.length
 
 if __name__ == '__main__':
     print('Welcome to Line Comparison Computation')
@@ -26,10 +31,17 @@ if __name__ == '__main__':
     point1 = Point(2, 5)
     point2 = Point(4, 6)
 
+    point3 = Point(3, 7)
+    point4 = Point(4, 8)
     line1 = Line(point1, point2)
-    line2 = Line(point1, point2)
+    line2 = Line(point3, point4)
 
-    print(line1 == line2)
+    if(line1 == line2):
+        print("Both lines are of equal length")
+    elif(line1 > line2):
+        print("Line 1 is greater than line 2")
+    elif(line1 < line2):
+        print("Line 2 is greater than line 1")
 
     print(f"Length of the line-1: {line1.length:.2f}")
     print(f"Length of the line-2: {line2.length:.2f}")
