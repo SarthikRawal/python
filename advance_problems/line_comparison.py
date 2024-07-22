389b00a6ac1e773e10792ed632f1c32ffc88f845
sarthik <sarthik32@gmail.com>
2024-07-22 15:00:16 +0530

    [Sarthik]ADD: UC-1 create points and calculate length of the line

---
+++ advance_problems/line_comparison.py
@@ -0,0 +1,30 @@
+# Line Comparison Computation
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    @property
+    def length(self):
+        return math.sqrt((self.point2.x - self.point1.x)**2 + (self.point2.y - self.point1.y)**2)
+    
+
+if __name__ == '__main__':
+    print('Welcome to Line Comparison Computation')
+
+    point1 = Point(2, 5)
+    point2 = Point(4, 6)
+
+    line1 = Line(point1, point2)
+
+
+    print(f"Length of the line-1: {line1.length:.2f}")
+    
